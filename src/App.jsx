7da0895900082d742b32dc798fe2f6885b242db7
defilippi82 <f.defilippi@gmail.com>
2024-05-12 20:34:39 -0300

    soluciono lo de colapsar menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Reservas } from "./components/Reservas";
 import { Socios } from "./components/Socios";
 import { Login } from "./components/Login";
@@ -12,39 +16,21 @@ export const App = () => {
     <div className="App container ">
       <BrowserRouter>
         <header>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-              <Link className="navbar-brand" href="/">
-                Club Social
-              </Link>
-              
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarText"
-                aria-controls="navbarText"
-                aria-expanded="false"
-                aria-label="Toggle navigation" >
-                <span className="navbar-toggler-icon"></span>
-              </button>
 
-              <div className="collapse navbar-collapse" id="navbarText">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link"><strong>Inicio</strong></Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link reservas" to="/reservas">
-                      <strong>Reservas</strong>
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link socios" to="/socios">
-                      <strong>Socios</strong>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+          <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+            <Navbar.Brand href="#home">Club Social</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="/reservas">Reservas</Nav.Link>
+            <Nav.Link href="/socios">Socios</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+
+</Container>
+          </Navbar>
         </header>
 
         <Routes>
